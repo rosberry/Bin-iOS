52c8e651aa17335fe11003024bfef96d3d5707f0
zhvrnkov <vlad.zhavoronkov@rosberry.com>
2021-07-29 19:28:40 +0600

    fix Package.swift manifest
    Add dependencies to it

--- Package.swift
+++ Package.swift
@@ -15,13 +15,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/rosberry/Deboogger", .branch("swift-package")),
+        .package(name: "Base", url: "https://github.com/rosberry/Base-iOS", .branch("master")),
+        .package(url: "https://github.com/efremidze/VisualEffectView", from: "4.1.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Bin",
-            dependencies: [],
+            dependencies: ["Deboogger", "Base", "VisualEffectView"],
             path: "Sources",
             exclude: ["AppConfiguration/README.md"]),
         .testTarget(
